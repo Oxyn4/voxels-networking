d482db686260974afa2e4536ac72f076248c15f8
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-02-25 01:25:37 +0000

    Change docs generation to doxygen -> breathe -> sphinx

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'The Voxels networking library'
+copyright = '2025, Jacob Evans'
+author = 'Jacob Evans'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["breathe"]
+
+breathe_projects = {
+    "voxels": "./generated/xml/"
+}
+
+breathe_default_project = "voxels"
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import better
+
+html_theme = 'alabaster'
+# html_theme_path = [better.better_theme_path]
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+.. The Voxels networking library documentation master file, created by
+   sphinx-quickstart on Sun Feb 23 23:41:42 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+The Voxels networking library documentation
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+
+.. doxygenpage:: Glossary
+   :content-only:
+
+
